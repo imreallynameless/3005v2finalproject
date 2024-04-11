56fea10b33f5d25e15dd65bac65fc0181516b063
Lei Wu <lw2002@hotmail.ca>
2024-04-10 21:57:25 -0400

    asdf

--- ddl.sql
+++ ddl.sql
@@ -42,8 +42,7 @@ CREATE TABLE trainers
         trainer_email VARCHAR(50) NOT NULL UNIQUE,
         trainer_password VARCHAR(50) NOT NULL,
         first_name VARCHAR(50) NOT NULL,
-        last_name VARCHAR(50) NOT NULL,
-        avaliable BOOLEAN NOT NULL
+        last_name VARCHAR(50) NOT NULL
     );
 
 CREATE TABLE administrators
